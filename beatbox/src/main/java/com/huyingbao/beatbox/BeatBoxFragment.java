26ad631dbcea3618b7f9be212d466c702708a174
K0170016 <K0170016@test.htsc.com.cn>
2018-10-24 17:46:06 +0800

    Android编程权威指南第20章内容

---
+++ beatbox/src/main/java/com/huyingbao/beatbox/BeatBoxFragment.java
@@ -0,0 +1,12 @@
+package com.huyingbao.beatbox;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by liujunfeng on 2018/10/24.
+ */
+public class BeatBoxFragment extends Fragment {
+    public static BeatBoxFragment newInstance(){
+        return new BeatBoxFragment();
+    }
+}
