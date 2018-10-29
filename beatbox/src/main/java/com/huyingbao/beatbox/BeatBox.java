ce36aaf8790f000e118d752486ff70401229a52b
K0170016 <K0170016@test.htsc.com.cn>
2018-10-29 17:31:21 +0800

    Android编程权威指南第21章内容

--- beatbox/src/main/java/com/huyingbao/beatbox/BeatBox.java
+++ beatbox/src/main/java/com/huyingbao/beatbox/BeatBox.java
@@ -1,7 +1,10 @@
 package com.huyingbao.beatbox;
 
 import android.content.Context;
+import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.util.Log;
 
 import java.io.IOException;
@@ -23,13 +26,20 @@ public class BeatBox {
     private static final String TAG = "BeatBox";
     //存储声音资源文件目录名
     private static final String SOUNDS_FOLODERS = "sample_sounds";
+    private static final int MAX_SOUNDS = 5;
 
     private AssetManager mAssetManager;
     private List<Sound> mSounds = new ArrayList<>();
+    private SoundPool mSoundPool;
 
     public BeatBox(Context context) {
         //所有Context中的AssetManager都管理着同一套assets资源
         mAssetManager = context.getAssets();
+        //创建SoundPool
+        mSoundPool = new SoundPool(
+                MAX_SOUNDS,//指定同时播放多少个音频
+                AudioManager.STREAM_MUSIC,//音频流类型，STREAM_MUSIC是音乐和游戏常用的音量控制常量
+                0);
         loadSounds();
     }
 
@@ -49,12 +59,36 @@ public class BeatBox {
             for (String filename : soundNames) {
                 String assetPath = SOUNDS_FOLODERS + "/" + filename;
                 Sound sound = new Sound(assetPath);
+                load(sound);//载入全部音频文件
                 mSounds.add(sound);
             }
         } catch (IOException e) {
             Log.e(TAG, "Could not list assets", e);
             e.printStackTrace();
         }
+    }
+
+    /**
+     * 加载音频文件
+     *
+     * @param sound
+     * @throws IOException
+     */
+    private void load(Sound sound) throws IOException {
+        //Asset文件修饰符
+        AssetFileDescriptor assetFileDescriptor = mAssetManager.openFd(sound.getAssetPath());
+        //load()方法可以把文件载入SoundPool待播。
+        //为了方便管理、重播或卸载音频文件，load()方法会返回一个int型的ID
+        //这实际就是存储在mSoundId中的ID
+        int soundId = mSoundPool.load(assetFileDescriptor, 1);
+        sound.setSoundId(soundId);
+    }
+
+    public void play(Sound sound) {
+        Integer soundId = sound.getSoundId();
+        if (soundId == null) return;
+        //音频ID、左音量、右音量、优先级、是否循环、播放速率
+        mSoundPool.play(soundId, 0.1f, 0.1f, 1, 0, 1.0f);
     }
 
     public List<Sound> getSounds() {
