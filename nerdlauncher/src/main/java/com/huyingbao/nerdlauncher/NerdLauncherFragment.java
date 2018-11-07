19d7610e27460ea9025468bd5d47746b7b507113
K0170016 <K0170016@test.htsc.com.cn>
2018-11-07 17:40:17 +0800

    Android编程权威指南第24章内容

---
+++ nerdlauncher/src/main/java/com/huyingbao/nerdlauncher/NerdLauncherFragment.java
@@ -0,0 +1,33 @@
+package com.huyingbao.nerdlauncher;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class NerdLauncherFragment extends Fragment {
+    private RecyclerView mRecyclerView;
+
+    public static NerdLauncherFragment newInstance() {
+        return new NerdLauncherFragment();
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_nerd_launcher, container, false);
+        mRecyclerView = view.findViewById(R.id.app_recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        return view;
+    }
+
+}
---
+++ nerdlauncher/src/main/java/com/huyingbao/nerdlauncher/SingleFragmentActivity.java
@@ -0,0 +1,38 @@
+package com.huyingbao.nerdlauncher;
+
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    protected abstract Fragment createFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResId());
+        //1:管理fragment队列
+        //2:管理fragment事务回退栈
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        //从fragment队列中获取资源ID标识的fragment
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+        if (fragment == null) {
+            fragment = createFragment();
+            //fragment事务被用来添加,移除,附加,分离或替换fragment队列中的fragment
+            //资源ID标识UI fragment是FragmentManager的一种内部实现机制
+            //添加fragment供FragmentManager管理时
+            //onAttach(Context),onCreate(Bundle)和onCreateView(...)方法会被调用
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+
+    @LayoutRes
+    protected int getLayoutResId() {
+        return R.layout.activity_fragment;
+    }
+}
